14424edd4c0c9bd517326fb8432524a8128d11f3
Ayesiza Hawah <yesiha2001@gmail.com>
2022-07-17 10:44:47 +0300

    create client side

---
+++ src/components/Comments.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import styled from "styled-components";
+import smallImage from "../images/mytybe.jpg";
+import Comment from "./Comment";
+
+const Container = styled.div``;
+const NewComment = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+`;
+const Avatar = styled.img`
+  width: 40px;
+  height: 40px;
+  border-radius: 50px;
+  background-color: #999;
+`;
+const Input = styled.input`
+  border: none;
+  border-bottom: 1px solid ${({ theme }) => theme.soft};
+  background-color: transparent;
+  outline: none;
+  padding: 5px;
+  width: 100%;
+`;
+
+const Comments = () => {
+  return (
+    <Container>
+      <NewComment>
+        <Avatar src={smallImage} />
+        <Input placeholder="Add a comment...." />
+      </NewComment>
+      <Comment />
+      <Comment />
+      <Comment />
+      <Comment />
+      <Comment />
+      <Comment />
+      <Comment />
+    </Container>
+  );
+};
+
+export default Comments;
